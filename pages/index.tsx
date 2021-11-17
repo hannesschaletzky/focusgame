3acce2ebf63fd11ec9b0c436565051c0bfade2b1
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2021-11-17 18:49:38 +0100

    init items panel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,46 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
-import { Button } from '@/styles/UI_Elements'
+import { StartButton, Item } from '@/styles/UI_Elements';
+
+// import { useState } from 'react';
+// const [name, setName] = useState("");
+// import { Input, Button } from '@/styles/UI_Elements'
+// <div className="flex flex-col justify-center items-center w-screen h-screen">
+//         <div className="mb-2">Enter a name:</div>
+//         <Input type="text" value={name} onChange={(e) => setName(e.target.value.trim())} />
+//         {name.length > 0 &&
+//           <Button href={"/game?name=" + name}>Let's play</Button>
+//         }
+//       </div>
 
 const Home: NextPage = () => {
+
+  
+
+  const getItems = ():JSX.Element[] => {
+    let items:JSX.Element[] = []
+    Array.from(Array(100)).forEach((x, i) => {
+      items.push(
+        <Item>{i + 1}</Item>
+      )
+    });
+    return items
+  }
+
   return (
-    <div>
+    <>
       <Head>
-        <title>Amazing Boilerplate</title>
+        <title>Focus</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="bg-gray-500">
-        HELLO WORLD
+      
+      <StartButton>Start</StartButton>
+
+      <div className="flex justify-center items-center flex-wrap w-screen h-screen lg:border">
+        {getItems()}
       </div>
-      <Button>
-        Press me...
-      </Button>
-    </div>
+    </>
   )
 }
 
--- styles/UI_Elements.tsx
+++ styles/UI_Elements.tsx
@@ -1,14 +1,54 @@
 import styled from "styled-components";
 
-export const Button = styled.div`
-  width: 150px;
-  height: 50px;
-  background-color: lightgray;
+export const Input = styled.input`
+  background-color: #f1f1f1;
+  padding: 1rem;
   border-radius: 8px;
-  color: black;
-  box-shadow: 2px 2px black;
-  
-  &:hover {
+  outline: none;
+`;
+
+export const Button = styled.a`
+  margin-top: 1rem;
+  padding: 1rem;
+  background-color: #1e911e;
+  color: white;
+  box-shadow: 2px 2px darkgray;
+  border-radius: 8px;
+  cursor: pointer;
+
+  &:active {
     box-shadow: none;
   }
+`;
+
+export const StartButton = styled.div`
+  position: absolute;
+  top: 40%;
+  left: 0; 
+  right: 0; 
+  margin-left: auto; 
+  margin-right: auto; 
+  height: 50px;
+  width: 100px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: orange;
+  color: white;
+  font-weight: bold;
+  border-radius: 8px;
+  cursor: pointer;
+  box-shadow: 1px 1px gray;
+
+  &:active {
+    box-shadow: none;
+  }
+`;
+
+export const Item = styled.div`
+  width: 20%;
+  height: calc(100vh/20);
+  background-color: lightgreen;
 `;
